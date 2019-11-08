ac0bfe1a84ad2da3898df5b6140b00d93f0e76c1
Nils Heumer <Nils.Heumer@quinscape.de>
2019-11-08 14:23:34 +0100

    Customer crud repository
    - Formatted code.

--- src/main/java/com/nilsign/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/nilsign/springbootdemo/SpringBootDemoApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootDemoApplication.class, args);
+  }
 }
