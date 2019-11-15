061916a02646fe0109d995e894045b29ae6632c1
Nils Heumer <nheumer@gmail.com>
2019-11-15 20:07:54 +0100

    Dtos
    - Adds an DTO layer for communication to a potential FE.
    - DB communication stays with entity object.

--- src/main/java/com/nilsign/springbootdemo/api/AddressController.java
+++ src/main/java/com/nilsign/springbootdemo/api/AddressController.java
@@ -1,5 +1,6 @@
 package com.nilsign.springbootdemo.api;
 
+import com.nilsign.springbootdemo.dto.AddressDto;
 import com.nilsign.springbootdemo.entity.AddressEntity;
 import com.nilsign.springbootdemo.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +9,41 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/address")
-public class AddressController extends AbstractController<AddressEntity, Long> {
+public class AddressController extends AbstractController<AddressDto, AddressEntity, Long> {
   @Autowired
   private AddressService addressService;
 
   @Override
   protected AddressService getService() {
     return addressService;
+  }
+
+  @Override
+  protected AddressEntity entityFromDto(AddressDto dto) {
+    return AddressController.addressEntityFromDto(dto);
+  }
+
+  @Override
+  protected AddressDto dtoFromEntity(AddressEntity entity) {
+    return AddressController.addressDtoFromEntity(entity);
+  }
+
+  public static AddressDto addressDtoFromEntity(AddressEntity entity) {
+    return new AddressDto(
+        entity.getId(),
+        entity.getAddress(),
+        entity.getCity(),
+        entity.getZip(),
+        entity.getCountry());
+  }
+
+  public static AddressEntity addressEntityFromDto(AddressDto dto) {
+    AddressEntity entity = new AddressEntity();
+    entity.setId(dto.getId());
+    entity.setAddress(dto.getAddress());
+    entity.setCity(dto.getCity());
+    entity.setZip(dto.getZip());
+    entity.setCountry(dto.getCountry());
+    return entity;
   }
 }
