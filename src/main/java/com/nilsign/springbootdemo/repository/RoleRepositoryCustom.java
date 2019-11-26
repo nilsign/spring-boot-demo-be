c1cc20326f4e79c40b5132c645995d948a4fa1a1
Nils Heumer <nheumer@gmail.com>
2019-11-26 12:39:49 +0100

    Dev Data
    - Deletes a TODO and adds to comments.

--- src/main/java/com/nilsign/springbootdemo/repository/RoleRepositoryCustom.java
+++ src/main/java/com/nilsign/springbootdemo/repository/RoleRepositoryCustom.java
@@ -6,6 +6,11 @@ import com.nilsign.springbootdemo.entity.RoleType;
 import java.util.Optional;
 
 public interface RoleRepositoryCustom {
+  // Note, that the implementation of this function will be automatically generated when this
+  // signature is added directly to the UserRepository. There is actually no need to create a
+  // UserRepositoryCustom interface and a UserRepositoryCustomImpl class for the findByEmail
+  // database request, but for the sake of creating a database request with the query builder and
+  // the entity manager this implementation is kept until there are more specific examples that can
+  // not be auto-generated.
   Optional<RoleEntity> findByRoleType(RoleType roleType);
 }
-
--- src/main/java/com/nilsign/springbootdemo/repository/UserRepositoryCustom.java
+++ src/main/java/com/nilsign/springbootdemo/repository/UserRepositoryCustom.java
@@ -5,7 +5,11 @@ import com.nilsign.springbootdemo.entity.UserEntity;
 import java.util.Optional;
 
 public interface UserRepositoryCustom {
-  // TODO(nilsheumer): Check whether Hibernate automatically implements the correct function, when
-  // the below function signature is just added to the plain UserRepository.java interface.
+  // Note, that the implementation of this function will be automatically generated when this
+  // signature is added directly to the UserRepository. There is actually no need to create a
+  // UserRepositoryCustom interface and a UserRepositoryCustomImpl class for the findByEmail
+  // database request, but for the sake of creating a database request with the query builder and
+  // the entity manager this implementation is kept until there are more specific examples that can
+  // not be auto-generated.
   Optional<UserEntity> findByEmail(String email);
 }
